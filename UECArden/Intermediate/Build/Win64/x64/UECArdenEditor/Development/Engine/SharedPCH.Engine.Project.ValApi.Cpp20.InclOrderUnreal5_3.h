36fcec43c61cdbe33045bc32128217a729814a78
demetrao0 <64824196+demetrao0@users.noreply.github.com>
2025-11-12 22:06:18 -0600

    reinicio
    a

--- UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Engine/SharedPCH.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Engine/Public/EngineSharedPCH.h
-#include "D:/Josep/Documents/Proyectos Unreal/UnrealEngineCArden2025/UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Engine/SharedDefinitions.Engine.Project.ValApi.Cpp20.InclOrderUnreal5_3.h"
-#include "Runtime/Engine/Public/EngineSharedPCH.h"
--- UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "D:/Josep/Documents/Proyectos Unreal/UnrealEngineCArden2025/UECArden/Intermediate/Build/Win64/x64/UECArdenEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
